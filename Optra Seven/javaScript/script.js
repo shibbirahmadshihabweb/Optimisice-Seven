cc4c9dba197599071ee3bf649a6fcf9bb39d5e8d
Jahangir Alam <jahangir025@gmail.com>
2025-08-24 04:13:41 +0600

    responsive menu design

--- Optra Seven/javaScript/script.js
+++ Optra Seven/javaScript/script.js
@@ -20,6 +20,7 @@ accordionItems.forEach(item => {
   });
 });
 
+
 /* Script For add class in sticky header*/
 window.addEventListener("scroll", function() {
   const header = document.querySelector(".site-main-header__container");
@@ -27,3 +28,41 @@ window.addEventListener("scroll", function() {
 });
 
 
+
+// responsive aside menu
+
+const responsiveMenuButton = document.querySelector(".main-menu__responsive-btn");
+const responsiveMenu = document.querySelector(".main-menu-responsive");
+const responsiveMenuClose = document.querySelector(".main-menu-responsive__close-btn");
+const responsiveMenuStartProjet = document.querySelector(".main-menu-responsive .start-project-btn-wrarpper");
+const responsiveMenuLogo = document.querySelector(".main-menu-responsive__logo");
+
+responsiveMenuButton.addEventListener('click', () => {
+  responsiveMenu.classList.add("active");
+  document.body.classList.add("no-scroll");
+});
+
+responsiveMenuClose.addEventListener('click', () => {
+  responsiveMenu.classList.remove("active");
+  document.body.classList.remove("no-scroll");
+});
+
+responsiveMenuStartProjet.addEventListener('click', () => {
+  responsiveMenu.classList.remove("active");
+  document.body.classList.remove("no-scroll");
+});
+
+responsiveMenuLogo.addEventListener('click', () => {
+  responsiveMenu.classList.remove("active");
+  document.body.classList.remove("no-scroll");
+});
+
+
+
+
+document.querySelectorAll(".main-menu-responsive .main-menu__link").forEach(link => {
+  link.addEventListener("click", () => {
+    responsiveMenu.classList.remove("active");
+   document.body.classList.remove("no-scroll");
+  });
+});
