a3ebe272de31d606fd73c4f252ef7ba34dd166b8
suhid98 <suhid17@gmail.com>
2025-08-25 18:44:36 +0600

    initial button component, some megamenu modification

--- Optra Seven/javaScript/megamenu.js
+++ Optra Seven/javaScript/megamenu.js
@@ -4,7 +4,7 @@ const megamenus = document.querySelectorAll(".megamenu");
 const megamenu_wrappers = document.querySelectorAll(".megamenu__content-wrapper");
 
 megamenus.forEach((menu) => {
-    menu.onmouseover = () => {
+    menu.click = () => {
         menu_background.classList.remove("hidden");
     }
 });
@@ -14,3 +14,22 @@ megamenu_wrappers.forEach((menu_wrappers) => {
         menu_background.classList.add("hidden");
     }
 });
+
+// const observer = new MutationObserver(() => {
+//   const display = window.getComputedStyle(target).display;
+
+//   if (display !== "none") {
+//     console.log("Element is now visible!");
+//     // Do something when it becomes visible
+//     link.href = "https://example.com"; // set or change link
+//   } else {
+//     console.log("Element is hidden.");
+//     // Optionally do something when hidden
+//     link.href = "#";
+//   }
+// });
+
+// observer.observe(target, {
+//   attributes: true,
+//   attributeFilter: ["style"],
+// });
