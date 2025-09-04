c62a069d9cc727d65ff1351ab32f6868700c65bf
suhid98 <suhid17@gmail.com>
2025-09-04 11:35:56 +0600

    before merge

--- Optra Seven/javaScript/style-guide.js
+++ Optra Seven/javaScript/style-guide.js
@@ -1,5 +1,11 @@
 const testSelect = document.getElementById("test-select");
+const testSelect2 = document.getElementById("test-select-2");
 const choices = new Choices(testSelect, {
   searchEnabled: true,
+  // searchEnabled: false,
+});
+const choices2 = new Choices(testSelect2, {
+  searchEnabled: true,
+  // searchEnabled: false,
 });
 
